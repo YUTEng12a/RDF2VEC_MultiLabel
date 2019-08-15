4c7339fbde841ce211c7a225c339cc1c2976a2fe
niuyuhang03 <niuyuhang03@qq.com>
2019-08-16 01:19:21 +0800

    fix bugs

--- data_manager.py
+++ data_manager.py
@@ -10,5 +10,5 @@ class data_manager:
         if dataset == 'cora':
             vectors = pd.read_csv(vectors_file, "\t", header=None)
         else:
-            vectors = pd.read_csv(vectors_file, "\t", header=None).values()[:, 1:]
+            vectors = pd.read_csv(vectors_file, "\t", header=None).loc[:, 1:]
         return vectors
