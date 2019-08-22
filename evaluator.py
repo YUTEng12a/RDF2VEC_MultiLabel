f640508daba88aba3b6aa0fd51b4963d79634b35
niuyuhang03 <niuyuhang03@qq.com>
2019-08-22 18:43:24 +0800

    fix bugs

--- evaluator.py
+++ evaluator.py
@@ -4,46 +4,46 @@ import data_manager
 import model
 
 
-# print("--------------------processing cora data: NB--------------------")
-#
-# dataset = 'cora'
-# vectors_file = './data/{}/{}.content'.format(dataset, dataset)
-#
-# # classification=0;regression=1
-# # in case of regression, neg_mean_squared_error is used; to calculate RMSE simply calculate the root
-# task = 0
-# # model option: NB, SVM, KNN, LR, M5
-# modelName = "NB"
-#
-# # data manager
-# data = data_manager.data_manager.readData(vectors_file, dataset)
-# data = data.sample(frac=1).reset_index(drop=True)
-#
-# # initialize the model
-# model1 = model.Model(task, modelName)
-# # train and print score
-# model1.train(data)
-#
-#
-# print("--------------------processing cora data: SVM--------------------")
-#
-# dataset = 'cora'
-# vectors_file = './data/{}/{}.content'.format(dataset, dataset)
-#
-# # classification=0;regression=1
-# # in case of regression, neg_mean_squared_error is used; to calculate RMSE simply calculate the root
-# task = 0
-# # model option: NB, SVM, KNN, LR, M5
-# modelName = "SVM"
-#
-# # data manager
-# data = data_manager.data_manager.readData(vectors_file, dataset)
-# data = data.sample(frac=1).reset_index(drop=True)
-#
-# # initialize the model
-# model1 = model.Model(task, modelName)
-# # train and print score
-# model1.train(data)
+print("--------------------processing cora data: NB--------------------")
+
+dataset = 'cora'
+vectors_file = './data/{}/{}.content'.format(dataset, dataset)
+
+# classification=0;regression=1
+# in case of regression, neg_mean_squared_error is used; to calculate RMSE simply calculate the root
+task = 0
+# model option: NB, SVM, KNN, LR, M5
+modelName = "NB"
+
+# data manager
+data = data_manager.data_manager.readData(vectors_file, dataset)
+data = data.sample(frac=1).reset_index(drop=True)
+
+# initialize the model
+model1 = model.Model(task, modelName)
+# train and print score
+model1.train(data)
+
+
+print("--------------------processing cora data: SVM--------------------")
+
+dataset = 'cora'
+vectors_file = './data/{}/{}.content'.format(dataset, dataset)
+
+# classification=0;regression=1
+# in case of regression, neg_mean_squared_error is used; to calculate RMSE simply calculate the root
+task = 0
+# model option: NB, SVM, KNN, LR, M5
+modelName = "SVM"
+
+# data manager
+data = data_manager.data_manager.readData(vectors_file, dataset)
+data = data.sample(frac=1).reset_index(drop=True)
+
+# initialize the model
+model1 = model.Model(task, modelName)
+# train and print score
+model1.train(data)
 
 print("--------------------processing FB15K237 data: mult NB--------------------")
 
