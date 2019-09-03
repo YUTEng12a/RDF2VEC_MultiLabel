c6ff6be8f240c4c8cae93e13a32c8ee950b792ff
niuyuhang03 <niuyuhang03@qq.com>
2019-09-03 10:02:09 +0800

    fix bugs about dele

--- data_manager.py
+++ data_manager.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+import os
+
 
 class data_manager:
     def __init__(self):
@@ -10,5 +12,19 @@ class data_manager:
         if dataset == 'cora':
             vectors = pd.read_csv(vectors_file, "\t", header=None)
         else:
-            vectors = pd.read_csv(vectors_file, "\t", header=None).loc[:, 1:]
+            if os.path.exists("./data/{}/{}.dele".format(dataset, dataset)):
+                idx_features_labels = np.genfromtxt("./data/{}/{}.content".format(dataset, dataset), dtype=np.dtype(str))
+                names = idx_features_labels[:, 0]
+                idx = np.array(idx_features_labels[:, 1], dtype=np.int32)
+                idx_map = {j: i for i, j in enumerate(idx)}
+                delete_entities_names = np.genfromtxt("./data/{}/{}.dele".format(dataset, dataset), dtype=np.dtype(str))
+                delete_entities_arg = np.array([np.where(names == ent_names)[0][0] for ent_names in delete_entities_names])
+                delete_entities_idx = list(map(idx_map.get, delete_entities_arg))
+                new_idx = []
+                for index in range(len(idx_map)):
+                    if not index in delete_entities_idx:
+                        new_idx.append(index)
+                vectors = pd.read_csv(vectors_file, "\t", header=None).loc[new_idx, 1:]
+            else:
+                vectors = pd.read_csv(vectors_file, "\t", header=None).loc[:, 1:]
         return vectors
--- evaluator.py
+++ evaluator.py
@@ -27,18 +27,7 @@ model1.train(data)
 
 print("--------------------processing cora data: SVM--------------------")
 
-dataset = 'cora'
-vectors_file = './data/{}/{}.content'.format(dataset, dataset)
-
-# classification=0;regression=1
-# in case of regression, neg_mean_squared_error is used; to calculate RMSE simply calculate the root
-task = 0
-# model option: NB, SVM, KNN, LR, M5
 modelName = "SVM"
-
-# data manager
-data = data_manager.data_manager.readData(vectors_file, dataset)
-data = data.sample(frac=1).reset_index(drop=True)
 
 # initialize the model
 model1 = model.Model(task, modelName)
@@ -68,18 +57,7 @@ model1.train(data, True)
 
 print("--------------------processing FB15K237 data: mult SVM--------------------")
 
-dataset = 'FB15K237'
-vectors_file = './data/{}/{}.content'.format(dataset, dataset)
-
-# classification=0;regression=1
-# in case of regression, neg_mean_squared_error is used; to calculate RMSE simply calculate the root
-task = 0
-# model option: NB, SVM, KNN, LR, M5
 modelName = "MultiLabelSVM"
-
-# data manager
-data = data_manager.data_manager.readData(vectors_file, dataset)
-data = data.sample(frac=1).reset_index(drop=True)
 
 # initialize the model
 model1 = model.Model(task, modelName)
@@ -109,62 +87,9 @@ model1.train(data, True)
 
 print("--------------------processing WN18RR data: mult SVM--------------------")
 
-dataset = 'WN18RR'
-vectors_file = './data/{}/{}.content'.format(dataset, dataset)
-
-# classification=0;regression=1
-# in case of regression, neg_mean_squared_error is used; to calculate RMSE simply calculate the root
-task = 0
-# model option: NB, SVM, KNN, LR, M5
 modelName = "MultiLabelSVM"
 
-# data manager
-data = data_manager.data_manager.readData(vectors_file, dataset)
-data = data.sample(frac=1).reset_index(drop=True)
-
 # initialize the model
 model1 = model.Model(task, modelName)
 # train and print score
 model1.train(data, True)
-
-print("--------------------processing WN18RR_sub30000 data: mult NB--------------------")
-
-dataset = 'WN18RR_sub30000'
-vectors_file = './data/{}/{}.content'.format(dataset, dataset)
-
-# classification=0;regression=1
-# in case of regression, neg_mean_squared_error is used; to calculate RMSE simply calculate the root
-task = 0
-# model option: NB, SVM, KNN, LR, M5
-modelName = "MultiLabelNB"
-
-# data manager
-data = data_manager.data_manager.readData(vectors_file, dataset)
-data = data.sample(frac=1).reset_index(drop=True)
-
-# initialize the model
-model1 = model.Model(task, modelName)
-# train and print score
-model1.train(data, True)
-
-
-print("--------------------processing WN18RR_sub30000 data: mult SVM--------------------")
-
-dataset = 'WN18RR_sub30000'
-vectors_file = './data/{}/{}.content'.format(dataset, dataset)
-
-# classification=0;regression=1
-# in case of regression, neg_mean_squared_error is used; to calculate RMSE simply calculate the root
-task = 0
-# model option: NB, SVM, KNN, LR, M5
-modelName = "MultiLabelSVM"
-
-# data manager
-data = data_manager.data_manager.readData(vectors_file, dataset)
-data = data.sample(frac=1).reset_index(drop=True)
-
-# initialize the model
-model1 = model.Model(task, modelName)
-# train and print score
-model1.train(data, True)
-                        
